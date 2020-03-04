8131afc9db3918892846d8fbfdaf4087f2c4b052
huangxiaojing <huangxiaojingcn@gmail.com>
2020-03-04 21:30:49 +0800

    Feat: add react-native-vector-icons on Android.

--- android/app/src/main/java/com/github_rn/MainApplication.java
+++ android/app/src/main/java/com/github_rn/MainApplication.java
@@ -6,9 +6,12 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.List;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -19,14 +22,13 @@ public class MainApplication extends Application implements ReactApplication {
           return BuildConfig.DEBUG;
         }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
-        }
+          @Override
+          protected List<ReactPackage> getPackages() {
+              return Arrays.<ReactPackage>asList(
+                      new MainReactPackage(),
+                      new VectorIconsPackage()
+              );
+          }
 
         @Override
         protected String getJSMainModuleName() {
